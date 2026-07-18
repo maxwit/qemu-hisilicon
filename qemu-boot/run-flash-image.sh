8c4f46c1deb3284a045420f4da9b2dccdd7a5192
Conke Hu <conke.hu@maxwit.com>
2026-07-18 20:49:50 +0800

    fix $soc
    Signed-off-by: Conke Hu <conke.hu@maxwit.com>

--- qemu-boot/run-flash-image.sh
+++ qemu-boot/run-flash-image.sh
@@ -1,33 +1,35 @@
-#!/bin/sh
+#!/bin/sh -e
 
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(dirname "$0")"
 REPO_ROOT="$(dirname "$SCRIPT_DIR")"
 QEMU="$REPO_ROOT/qemu-src/build/qemu-system-arm"
 
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 <flash_image>"
+    echo "Usage: $0 <flash_image>"
 fi
 
 flash_image=$1
 
-case $flash_image in
-	*hi3516ev200*)
-		soc=hi3516ev200
-		;;
-	*hi3516ev300*)
-		soc=hi3516ev300
-		;;
-	*hi3518ev300*)
-		soc=hi3518ev300
-		;;
-	*)
-		echo "Unknow SoC Type!"
-		exit 1
-		;;
+case "$flash_image" in
+    *hi351[68][acde]v[0-9][0-9][0-9]*)
+        soc=$(basename $flash_image | grep -o hi351[68][acde]v[0-9][0-9][0-9])
+        ;;
+    *gk720[0-9]v[0-9][0-9][0-9]*)
+        soc=$(basename $file_image | grep -o gk720[0-9]v[0-9][0-9][0-9])
+        ;;
+    *sc6152*)
+        soc=gk7205v200
+        ;;
+    *)
+        echo "Unknow SoC Type!"
+        exit 1
+        ;;
 esac
 
+net_opt="-netdev tap,id=net0,ifname=tap0,script=no,downscript=no -net nic,netdev=net0"
+
 # TODO: add non-interactive test cases
-echo "### Running qemu-system-arm -M $soc ... ###"
-$QEMU -M $soc,flash-file=$flash_image \
-    -netdev tap,id=net0,ifname=tap0,script=no,downscript=no -net nic,netdev=net0 \
-    -nographic
+run_qemu="$QEMU -M $soc,flash-file=$flash_image -nographic $net_opt"
+
+echo "Running $run_qemu"
+$run_qemu
